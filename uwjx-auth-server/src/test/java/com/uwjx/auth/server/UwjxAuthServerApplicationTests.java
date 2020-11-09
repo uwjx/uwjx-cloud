51617123ac3f0ddfb4948661f19c9bbfe6e1ce6e
Hugh Wang <18501667737@163.com>
2020-11-09 14:33:25 +0800

    init

---
+++ uwjx-auth-server/src/test/java/com/uwjx/auth/server/UwjxAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.auth.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwjxAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
